bc654b9f107d2ea5cf89e7ed2e61e61a1eb716f3
OsukaTheThird <125950243+OsukaTheThird@users.noreply.github.com>
2025-02-12 21:54:09 -0600

    friends
    ksjfdksdjf

---
+++ src/components/chat/friends-item.tsx
@@ -0,0 +1,16 @@
+const FriendsItem = () => {
+  return (
+    <article className='flex items-center gap-x-3 py-2 px-4 border-b hover:bg-gray-200'>
+          <img 
+            src="https://randomuser.me/api/portraits/women/50.jpg" 
+            alt="" 
+            className='w-14 h-14 rounded-full'/>
+          <div>
+            <h3 className='font-semibold text-lg text-gray-800'>Clara Schürer</h3>
+            <p className='text-xs text-gray-800'>Lorem ipsum dolor sit amet...</p>
+          </div>
+        </article>
+  )
+}
+
+export default FriendsItem
--- src/components/chat/friends.tsx
+++ src/components/chat/friends.tsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import FriendsItem from "./friends-item"
 
 const Friends = () => {
   return (
     <div className='grid grid-rows-[auto_1fr]'>
-      <section>
+      <section className='border-b p-4'>
         <h2>Chats</h2>
          <div>Buscar amigos</div>
        </section>
-       <section className='bg bg-zinc-300'>Lista de amigos</section>
+      <section className=''>
+
+        {Array.from({length: 10}).map((_, i) => (
+          <FriendsItem key={i}/>
+        ))}
+        
+      </section>
     </div>
   )
 }
