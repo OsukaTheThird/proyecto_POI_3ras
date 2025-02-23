97f5ef350089c19e544bd3e0bf43618239041c88
OsukaTheThird <125950243+OsukaTheThird@users.noreply.github.com>
2025-02-23 15:55:34 -0600

    barra de menu
    se agrego una barra de menu/navbar pero aun no esta optimizada ni muestra algun icono

--- src/layout/chat-layout.tsx
+++ src/layout/chat-layout.tsx
@@ -1,10 +1,11 @@
 import Friends from "@/components/chat/friends"
 import Messages from "@/components/chat/messages"
 import Profile from "@/components/chat/profile"
+import SideBar from "./sidebar"
 
 const ChatLayout = () => {
   return (
-    <div className="grid grid-cols-[2fr_5fr_2fr] h-screen">
+    <div className="grid grid-cols-[2fr_5fr_2fr] h-screen flex-grow">
       <Friends />
       <Messages />
       <Profile />
