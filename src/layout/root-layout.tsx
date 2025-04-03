c57f719bc9bb6ced041c73415ed0a77d192f2610
KoroliKatus <alereyesjuarez1@gmail.com>
2025-04-02 22:21:06 -0600

    tienda

--- src/layout/root-layout.tsx
+++ src/layout/root-layout.tsx
@@ -4,6 +4,7 @@ import AuthLayout from "./auth-layout";
 import List from "./list-layout"; // Importa tu componente de tareas
 import { useSigninCheck } from "reactfire";
 import { useLoadingStore } from "@/store/loading-store";
+import Store from "./store-layout";
 
 const RootLayout = () => {
     const { status, data: signInCheckResult} = useSigninCheck();
@@ -20,6 +21,7 @@ const RootLayout = () => {
                     <Routes>
                         <Route path="/" element={<ChatLayout />} />
                         <Route path="/tareas" element={<List />} />
+                        <Route path="/tienda" element={<Store />} />
                     </Routes>
                 ) : (
                     <AuthLayout />
---
+++ src/layout/store-layout.tsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import Navbar from "./navbar"
+
+
+const products = [
+  {
+    id: 1,
+    name: "Ballenas estelares",
+    price: 79,
+    image: "assets/images/ballena.png",
+  },
+  {
+    id: 2,
+    name: "Vivir en las nubes",
+    price: 79,
+    image: "assets/images/nubes.png",
+  },
+  {
+    id: 3,
+    name: "Espejismo",
+    price: 79,
+    image: "assets/images/espejismo.png",
+  },
+];
+
+export default function Store() {
+  return (
+    <div className="h-screen">
+    <Navbar />
+
+    <div className="min-h-screen bg-white text-black p-10">
+      <h1 className="text-3xl font-bold mb-6">Tienda</h1>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        {products.map((product) => (
+          <Card key={product.id} className="bg-gray-300 p-4 rounded-xl shadow-lg">
+            <CardContent>
+              <img
+                src={product.image}
+                alt={product.name}
+                className="w-full h-40 object-cover rounded-lg"
+              />
+              <h2 className="mt-4 text-xl font-semibold">{product.name}</h2>
+              <p className="text-lg font-bold text-green-600">{product.price} MXN</p>
+              <div className="mt-4 flex gap-2">
+                <Button className="bg-white text-black p-2 rounded">
+                    Comprar
+                </Button>
+              </div>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+    </div>
+    </div>
+  );
+}
